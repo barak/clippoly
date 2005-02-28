f6f349ff79ccdee0ad2edfeb7808df49c4ee69e9
klamer <klamer>
2005-02-28 16:18:35 +0000

    PL6

---
+++ CC/String.cc
@@ -0,0 +1,48 @@
+static const char rcs_id[] = "$Header$";
+
+// $Log$
+// Revision 1.1  2005/02/28 16:23:46  klamer
+// PL6
+//
+
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include	<string.h>
+#include	<iostream.h>
+
+#include "String.h"
+
+static const char h_rcs_id[] = _String_h;
+
+String::~String()
+{
+  delete [] ptr;
+}
+
+String::String(void)
+    : ptr(new char[1])
+{
+  ptr[0] = 0;
+}
+
+int
+  String::operator!=(const char *cmp) const
+{
+  return strcmp(ptr,cmp);
+}
+
+istream &
+  operator>>(istream &in, String &s)
+{
+  char	buffer[256];
+  buffer[0] = 0;
+  in.get(buffer,256);
+  delete [] s.ptr;
+  s.ptr = new char [strlen(buffer) + 1];
+  strcpy(s.ptr,buffer);
+  return in;
+}
+
+
