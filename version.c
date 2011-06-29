af21d7cff6c7d7773048e162d2585a4576a6baba
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2011-06-29 21:40:51 +0200

    add symbol clippoly_version for convenience of autotools

---
+++ version.c
@@ -0,0 +1,4 @@
+// include unique non-C++ symbol for ease of ./configure tests
+#include "config.h"
+#include "version.h"
+const char clippoly_version[] = PACKAGE_VERSION;
---
+++ version.h
@@ -0,0 +1,1 @@
+extern const char clippoly_version[];
